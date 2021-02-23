a4faa347ef6d38684eef4339ed62407ab3f1221c
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2021-02-23 20:00:03 -0300

    feat: introduce initial project

---
+++ src/main/java/br/com/shell/springshell/SpringShellApplication.java
@@ -0,0 +1,13 @@
+package br.com.shell.springshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/shell/springshell/SpringShellApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.shell.springshell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
